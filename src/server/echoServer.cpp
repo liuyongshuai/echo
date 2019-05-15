acdd3ddc0580aec8ef2f879393265165b1ab1142
liuyongshuai <liuyongshuai@didichuxing.com>
2019-05-15 16:59:40 +0800

    moddify

---
+++ src/server/echoServer.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+ * File:	echoServer.cpp
+ * Author:	liuyongshuai<liuyongshuai@hotmail.com>
+ * Time:	2019-05-15 16:59
+ ************************************************************************/
+#include<iostream>
+#include "echoServer.h"
+
+using namespace std;
+
+namespace echo{
+
+} //namespace echo
---
+++ src/server/echoServer.h
@@ -0,0 +1,20 @@
+/*************************************************************************
+ * File:	echoServer.h
+ * Author:	liuyongshuai<liuyongshuai@hotmail.com>
+ * Time:	2019-05-15 16:59
+ ************************************************************************/
+#ifndef _ECHO_ECHOSERVER_H_201905151659_
+#define _ECHO_ECHOSERVER_H_201905151659_
+
+#include<string>
+#include<vector>
+#include<map>
+
+namespace echo{
+    class echoServer{
+
+    };
+} //namespace echo
+
+
+#endif //_ECHO_ECHOSERVER_H_201905151659_
