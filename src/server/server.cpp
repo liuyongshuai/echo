006683a017606b694fbe1c63266c0023290d6dca
liuyongshuai <liuyongshuai@didichuxing.com>
2019-05-16 20:56:51 +0800

    mo

---
+++ src/server/server.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+ * File:	server.cpp
+ * Author:	liuyongshuai<liuyongshuai@hotmail.com>
+ * Time:	2019-05-16 20:50
+ ************************************************************************/
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdint.h>
+
+int main(){
+    return 0;
+}
