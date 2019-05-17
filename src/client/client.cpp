a8f131c374ea1de020a2b1de7441d4f89a639f28
liuyongshuai@hotmail.com <liuyongshuai@hotmail.com>
2019-05-18 00:09:06 +0800

    s

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -3,12 +3,45 @@
  * Author:	liuyongshuai<liuyongshuai@hotmail.com>
  * Time:	2019-05-16 20:50
  ************************************************************************/
-#include<stdlib.h>
-#include<unistd.h>
-#include<string.h>
-#include<stdio.h>
-#include<stdint.h>
+#include "common.h"
 
-int main(){
+using namespace std;
+
+int main(int argc, char *argv[]){
+    string selfName = "我";
+    if (argc > 0 && strlen(argv[1]) > 0){
+        selfName = argv[1];
+    }
+    cout << selfName << endl;
+
+    char *input, shell_prompt[100] = {0};
+
+    // Configure readline to auto-complete paths when the tab key is hit.
+    rl_bind_key('\t', rl_complete);
+
+    for (;;){
+        // Create prompt string from user name and current working directory.
+        snprintf(shell_prompt, sizeof(shell_prompt), selfName.c_str());
+
+        // Display prompt and read input (n.b. input must be freed after use)...
+        input = readline(shell_prompt);
+
+        // Check for EOF.
+        if (!input){
+            break;
+        }
+
+        printf("aadfsaadsfasdf\t---%s\n", input);
+
+        // Add input to history.
+        add_history(input);
+
+        // Do stuff...
+
+        // Free input.
+        free(input);
+    }
+
+
     return 0;
 }
---
+++ src/client/common.h
@@ -0,0 +1,35 @@
+/*************************************************************************
+ * File:	common.h
+ * Author:	liuyongshuai<liuyongshuai@hotmail.com>
+ * Time:	2019-05-17 下午11:51
+ ************************************************************************/
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdint.h>
+#include<fcntl.h>
+#include<deque>
+#include<map>
+#include<vector>
+#include<pthread.h>
+#include<semaphore.h>
+#include<errno.h>
+#include<string>
+#include<signal.h>
+#include<cctype>
+#include<sstream>
+#include<utility>
+#include<stdexcept>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<iostream>
+#include<sys/epoll.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<sys/stat.h>
+#include<sys/time.h>
+#include<sys/shm.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "proto/echo.pb.h"
