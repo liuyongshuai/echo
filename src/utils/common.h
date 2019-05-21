12743a7483d3e6d17db03bb5723dd8f67f4b5c11
liuyongshuai <liuyongshuai@didichuxing.com>
2019-05-21 12:24:46 +0800

    s

---
+++ src/utils/common.h
@@ -0,0 +1,43 @@
+/*************************************************************************
+ * File:	common.h
+ * Author:	liuyongshuai<liuyongshuai@hotmail.com>
+ * Time:	2019-05-21 12:15
+ ************************************************************************/
+#ifndef _ECHO_COMMON_H_201905211215_
+#define _ECHO_COMMON_H_201905211215_
+
+#include<cstring>
+#include<stdint.h>
+#include<deque>
+#include<string>
+#include<stdlib.h>
+#include<errno.h>
+#include<fcntl.h>
+#include<sstream>
+#include<vector>
+#include<semaphore.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<signal.h>
+#include<cctype>
+#include<limits.h>
+#include<sys/epoll.h>
+#include<sys/socket.h>
+#include<sys/time.h>
+#include<sys/shm.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<mysql/mysql.h>
+#include<iostream>
+#include<utility>
+#include<stdexcept>
+#include<curl/curl.h>
+#include<string.h>
+#include<map>
+#include<pthread.h>
+#include<readline/history.h>
+#include<readline/readline.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+
+#endif //_ECHO_COMMON_H_201905211215_
